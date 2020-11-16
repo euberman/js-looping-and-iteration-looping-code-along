36f7823d8ffe70e97b804f9d53415a7f057975c8
euberman <euberman@gmail.com>
2020-11-16 14:51:56 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(aList, eventName){
+  let result = [];
+  for (let i = 0; i < aList.length; i++) { 
+    let newMessage = `Thank you, ${aList[i]}, for the wonderful surprise gift!`;
+    result.push(newMessage);
+  }
+  return result;
+};
+
+function countDown(num){
+  let i = num
+  while (i >= 0){
+    console.log(i--);
+  }
+};
